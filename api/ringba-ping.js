4c2db8b6474878e2bbcfefae6b19d19ec4ed0720
hamza2233-dev <ha4326491@gmail.com>
2026-09-03 09:55:20 -0700

    Update Ringba API endpoint URL

--- api/ringba-ping.js
+++ api/ringba-ping.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   };
 
   try {
-    const response = await fetch('https://rtb.ringba.com/v1/production/bf95a1c2fdfa4dc189c24360e9b05252.json', {
+    const response = await fetch('https://rtb.ringba.com/v1/production/720b79f59a9c4187aaf6565c339f0f6f.json', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
